17f107d3b8d1a183bf23b48d9b9c18ad32e504de
spikkie <spikkie@gmail.com>
2020-02-11 03:16:57 +0100

    ../docker/nginx/mysite.conf  change 3

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn", "static_root")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn", "static_root", "static/")
 
 STATICFILES_STORAGE = (
     "django.contrib.staticfiles.storage.StaticFilesStorage"
@@ -153,7 +153,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "static_cdn", "media_root")
 
-PROTECTED_ROOT = os.path.join(BASE_DIR, "static_cdn", "protected_media")
+PROTECTED_ROOT = os.path.join(BASE_DIR, "static_cdn", "protected_media", "media/")
 
 
 REST_FRAMEWORK = {
