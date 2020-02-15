db5edaaa14d7a575f4f89babf0f991131a2b0d8d
spikkie <spikkie@gmail.com>
2020-02-15 17:34:16 +0100

    no  gcloud config configurations activate

--- google_cloud_production/init.sh
+++ google_cloud_production/init.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -x
 
-gcloud config configurations activate cloudshell-32705
+gcloud config configurations list
+echo gcloud config configurations activate cloudshell-32705
 gcloud container clusters create geneesplaats-nl-cluster-1 --zone europe-west4-a --num-nodes=2
 
 
